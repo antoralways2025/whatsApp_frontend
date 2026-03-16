73008c1d14339427d52d9e61077bdab2db9235cb
Sheikh Antor <antoralways2025@gmail.comm>
2026-03-16 06:22:53 +0600

    new

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+const BACKEND_URL =
+  process.env.REACT_APP_BACKEND_URL ||
+  "https://whatsapp-backend-87dn.onrender.com";
 
 export const socket = io(BACKEND_URL, {
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
   withCredentials: true
 });
